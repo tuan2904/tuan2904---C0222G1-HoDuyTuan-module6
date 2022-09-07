6b131587b4d8cdd75363476714e29828a5d03e51
tuan2904 <dtuan290400@gmail.com>
2022-09-07 08:16:42 +0700

    update

--- Spring 2/Project_Spring2_FE/shop-product/src/app/app.module.ts
+++ Spring 2/Project_Spring2_FE/shop-product/src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooderComponent } from './fooder/fooder.component';
+import { ShopComponent } from './shop/shop.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import {APP_BASE_HREF} from '@angular/common';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooderComponent,
+    ShopComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
